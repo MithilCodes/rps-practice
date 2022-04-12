276c3263920e7881f7ad637aae9e363723a8b3a0
MithilCodes <102288170+MithilCodes@users.noreply.github.com>
2022-04-12 09:33:42 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rps_trial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Rocks,Papers and Scissors game");
+            
+            
+            {
+                Console.WriteLine("Select any one:\nROCK\nPAPER\nSCISSOR");
+                string[] choices = new string[3] { "ROCK", "PAPER", "SCISSOR" };
+                Random rnd = new Random();
+                int n = rnd.Next(0, 3);
+                Console.WriteLine("Enter your choice:");
+                string user = Console.ReadLine();
+                Console.WriteLine( choices[n]);
+
+                if (user == "ROCK" && choices[n] == "SCISSOR")
+                {
+                    Console.WriteLine("User wins");
+                  
+                }
+                else if (user == "ROCK" && choices[n] == "PAPER")
+                {
+                    Console.WriteLine("Computer wins");
+                    
+                }
+                else if (user == "PAPER" && choices[n] == "ROCK")
+                {
+                    Console.WriteLine("User wins");
+                    
+                }
+                else if (user == "PAPER" && choices[n] == "SCISSOR")
+                {
+                    Console.WriteLine("Computer Wins");
+                    
+                }
+                else if (user == "SCISSOR" && choices[n] == "ROCK")
+                {
+                    Console.WriteLine("Computer Wins");
+                    
+                }
+                else if (user == "SCISSOR" && choices[n] == "PAPER")
+                {
+                    Console.WriteLine("User wins");
+                   
+                }
+                else
+                {
+                    Console.WriteLine("Same choices");
+                }
+               
+            }
+           
+        }
+    }
+}
+       
+        
